06ed0a784bcc6af234804eb823dba159cbe288a0
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-07 02:06:50 -0500

    added error handling

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,5 +1,6 @@
 import * as userController from "../controllers/user-controller.js";
 import checkAuth from "../controllers/check-auth-controller.js";
+import * as invalidErrorController from "../controllers/invalidError-controller.js";
 import express from "express";
 
 const router = express.Router();
@@ -9,5 +10,8 @@ router.use("/user/self", checkAuth);
 router.route("/user/self")
   .get(userController.getUser)
   .put(userController.updateUser);
+
+router.all("*",
+  invalidErrorController.errorURL);
 
 export default router;
