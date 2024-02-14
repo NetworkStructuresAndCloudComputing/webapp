2f063a1cfaf5894054d02e0aded25e18b4ce0d3e
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-14 16:48:39 -0500

    adding username check for auth

--- backend/controllers/check-auth-controller.js
+++ backend/controllers/check-auth-controller.js
@@ -3,43 +3,49 @@ import bcrypt from 'bcrypt';
 import { setErrorResponse } from "./response-handler.js";
 
 const checkAuth = async (req, res, next) => {
-    if (!req.get('Authorization')) {
-        setErrorResponse('401', 'User is not authenticated to access the resource.', res);
-        return;
-    }
-    else {
+  if (!req.get('Authorization')) {
+      setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+      return;
+  } else {
       var credentials = Buffer.from(req.get('Authorization').split(' ')[1], 'base64')
-        .toString()
-        .split(':')
-      var username = credentials[0]
-      var password = credentials[1]
-        
-      const user =  await User.findAll({
-        where: {
-          username,
-        },
-      });
-      if(user.length > 0){
-      const userObject = user[0];
-      let dbpassword = userObject.dataValues.password;
+          .toString()
+          .split(':');
+      var username = credentials[0];
+      var password = credentials[1];
 
-bcrypt.compare(password, dbpassword, (err, result) => {
-    if(err){
-        next(err)
-    }
-    if(result){
-        req.username = username
-      next()
-    }
-    else{
-        setErrorResponse('401', 'User is not authenticated to access the resource.', res);
-        return;
-    }
-}) }
-else{
-  setErrorResponse('400', "User does not exist.", res);
+      try {
+          const user = await User.findAll({
+              where: {
+                  username,
+              },
+          });
 
-} }
+          if (user.length > 0) {
+              const userObject = user[0];
+              let dbpassword = userObject.dataValues.password;
+
+              bcrypt.compare(password, dbpassword, (err, result) => {
+                  if (err) {
+                      next(err);
+                  }
+                  if (result) {
+                      req.username = username;
+                      next();
+                  } else {
+                    setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+                      return;
+                  }
+              });
+          } else {
+            setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+              return;
+          }
+      } catch (error) {
+          console.error(error);
+          setErrorResponse('400', 'Bad Request', res);
+          return;
+      }
   }
+};
 
-export default checkAuth
+export default checkAuth;
