91fa4d3367659ea7c785eedec2637be5bff78b50
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-21 18:04:57 -0500

    adding assignment 4

---
+++ backend/webapp/backend/controllers/check-auth-controller.js
@@ -0,0 +1,51 @@
+import User from "../models/user.js"
+import bcrypt from 'bcrypt';
+import { setErrorResponse } from "./response-handler.js";
+
+const checkAuth = async (req, res, next) => {
+  if (!req.get('Authorization')) {
+      setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+      return;
+  } else {
+      var credentials = Buffer.from(req.get('Authorization').split(' ')[1], 'base64')
+          .toString()
+          .split(':');
+      var username = credentials[0];
+      var password = credentials[1];
+
+      try {
+          const user = await User.findAll({
+              where: {
+                  username,
+              },
+          });
+
+          if (user.length > 0) {
+              const userObject = user[0];
+              let dbpassword = userObject.dataValues.password;
+
+              bcrypt.compare(password, dbpassword, (err, result) => {
+                  if (err) {
+                      next(err);
+                  }
+                  if (result) {
+                      req.username = username;
+                      next();
+                  } else {
+                    setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+                      return;
+                  }
+              });
+          } else {
+            setErrorResponse('401', 'User is not authenticated to access the resource.', res);
+              return;
+          }
+      } catch (error) {
+          console.error(error);
+          setErrorResponse('400', 'Bad Request', res);
+          return;
+      }
+  }
+};
+
+export default checkAuth;
---
+++ backend/webapp/backend/controllers/healthz-controller.js
@@ -0,0 +1,22 @@
+
+import { sequelize } from '../services/database-service.js';
+import { setErrorResponse,setResponse } from './response-handler.js';
+
+export const healthzCheck = async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    res.status(200).send();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    res.status(503).send();
+  }
+};
+
+export const methodNotAllowed = (req, res) => {
+  res.status(405).send();
+};
+
+export const badRequest = (req, res) =>{
+  res.status(400).send();
+};
