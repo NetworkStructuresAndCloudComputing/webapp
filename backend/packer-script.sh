31d8f19c695961e8776f13fa571063f6f97a129a
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-01 15:45:48 -0500

    removed sql scripts

--- backend/packer-script.sh
+++ backend/packer-script.sh
@@ -7,12 +7,5 @@ sudo yum -y install curl
 curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
 sudo yum -y install nodejs
 
-# Install MySQL server
-sudo yum -y install mysql-server
-
-sudo systemctl enable --now mysqld
-
-sudo mysql --connect-expired-password -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '2108786Z@kir';"
-
 sudo yum -y install unzip
 
