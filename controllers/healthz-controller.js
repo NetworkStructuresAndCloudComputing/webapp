cfc4e989298cb7ed6859af8bed74eb64484cec21
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-17 01:27:11 -0400

    test 7

--- controllers/healthz-controller.js
+++ controllers/healthz-controller.js
@@ -1,11 +1,14 @@
 
 import { sequelize } from '../services/database-service.js';
 import { setErrorResponse,setResponse } from './response-handler.js';
+import Logger  from 'node-json-logger';
+const logger = new Logger();
 
 export const healthzCheck = async (req, res) => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    logger.info('Success Message');
     res.status(200).send();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
