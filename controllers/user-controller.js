bab196fe8391ee72ada7e0898cc08c2f67d57f19
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-19 00:20:18 -0400

    adding new logs

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,53 +4,46 @@ import Logger  from 'node-json-logger';
 
 import logger from '../logger.js'
 
-// const logger = new Logger();
-
-
-
 export const getUser = async (request, response) => {
   try {
     const { username } = request;
     const user = await userService.searchByEmail({ username });
 
     if (!user) {
+      setErrorResponse('404','User not found', response);
       logger.warn({
         severity: "WARNING",
         message: "User not found.",
-        spanId: request.spanId,
         httpRequest: {
           method: request.method,
-          status: request.statusCode,
+          status: response.statusCode,
           url: request.originalUrl,
         }
       });
-      setErrorResponse('404','User not found', response);
       return;
     }
+    setResponse(user, response);
     logger.info({
       severity: "INFO",
       message: "User fetched successfully",
-      spanId: request.spanId,
       httpRequest: {
         method: request.method,
-        status: request.statusCode,
+        status: response.statusCode,
         url: request.originalUrl,
       }
     });
-    setResponse(user, response);
   } catch (e) {
     console.error(e);
+    setErrorResponse('404', "Bad Request", response);
     logger.warn({
       severity: "WARNING",
       message: "Bad Request.",
-      spanId: req.spanId,
       httpRequest: {
         method: request.method,
-        status: request.statusCode,
+        status: response.statusCode,
         url: request.originalUrl,
       }
     });
-    setErrorResponse('404', "Bad Request", response);
   }
 };
 
@@ -59,44 +52,41 @@ export const getUser = async (request, response) => {
         const params = { ...request.body };
         const existingUser = await userService.searchByEmail({ username: params.username });
         if(existingUser) {
-          logger.warn({
-            severity: "WARNING",
-            message: "User already exists.",
-            spanId: request.spanId,
-            httpRequest: {
-              method: request.method,
-              status: request.statusCode,
-              url: request.originalUrl,
-            }
-          });
             setErrorResponse('400','User already exists', response);
-        } else {
-            const newUser = await userService.create(params);
-            logger.info({
-              severity: "INFO",
-              message: "user created successfully",
-              spanId: request.spanId,
+            logger.warn({
+              severity: "WARNING",
+              message: "User already exists.",
               httpRequest: {
                 method: request.method,
-                status: request.statusCode,
+                status: response.statusCode,
                 url: request.originalUrl,
               }
             });
+        } else {
+            const newUser = await userService.create(params);
             setResponsefor201(newUser, response);
+            logger.info({
+              severity: "INFO",
+              message: "user created successfully",
+              httpRequest: {
+                method: request.method,
+                status: response.statusCode,
+                url: request.originalUrl,
+              }
+            });
         }
     } catch (error) {
         console.error(error);
+        setErrorResponse('400','Invalid request, check the payload.', response);
         logger.warn({
           severity: "WARNING",
           message: "Invalid request, check the payload.",
-          spanId: request.spanId,
           httpRequest: {
             method: request.method,
-            status: request.statusCode,
+            status: response.statusCode,
             url: request.originalUrl,
           }
         });
-        setErrorResponse('400','Invalid request, check the payload.', response);
     }
 };
 
@@ -106,30 +96,28 @@ export const updateUser = async (request, response) => {
     const { username } = request;
     const params = { ...request.body };
     const updatedUser = await userService.update(params, username);
+    setResponsefor204(updatedUser, response);
     logger.info({
       severity: "INFO",
       message: "User updated successfully",
-      spanId: request.spanId,
       httpRequest: {
         method: request.method,
-        status: request.statusCode,
+        status: response.statusCode,
         url: request.originalUrl,
       }
     });
-    setResponsefor204(updatedUser, response);
   } catch (e) {
     console.error(e);
+    setErrorResponse('400', 'Invalid request, check the payload', response);
     logger.warn({
       severity: "WARNING",
       message: "Invalid request, check the payload.",
-      spanId: request.spanId,
       httpRequest: {
         method: request.method,
-        status: request.statusCode,
+        status: response.statusCode,
         url: request.originalUrl,
       }
     });
-    setErrorResponse('400', 'Invalid request, check the payload', response);
   }
 };
 
