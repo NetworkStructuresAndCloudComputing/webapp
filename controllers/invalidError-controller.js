4d39626b696b423c6d01f36b4ae70583d7bd6e8d
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-19 15:48:44 -0400

    adding one more logger

--- controllers/invalidError-controller.js
+++ controllers/invalidError-controller.js
@@ -1,3 +1,16 @@
+import Logger  from 'node-json-logger';
+
+import logger from '../logger.js'
+
 export const errorURL = async (req, res) => {
     res.status(400).send();
+    logger.warn({
+      severity: "WARNING",
+      message: "Bad request.",
+      httpRequest: {
+        method: req.method,
+        status: res.statusCode,
+        url: req.originalUrl,
+      }
+    });
   }
