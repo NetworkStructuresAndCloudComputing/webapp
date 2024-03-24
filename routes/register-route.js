1525b72f29fa5274fb477fece6b03bf2791ef368
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-24 17:23:15 -0400

    test 6

--- routes/register-route.js
+++ routes/register-route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import * as userController from '../controllers/user-controller.js';
 import * as invalidErrorController from "../controllers/invalidError-controller.js";
-import app from '../App.js';
 
 const router = express.Router();
 
