be5052f93645731908c34c50704d2f1119829319
MohammedZakirMemon <memon.m@northeastern.edu>
2024-03-26 19:21:22 -0400

    adding verify email

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -128,20 +128,45 @@ export const updateUser = async (request, response) => {
   }
 };
 
+export const verifyUser = async (req, res) => {
+  const {userId} = req.params
 
-export const verifyUser = async (request, response, next) => {
-  try {
-
-      const { username } = request;  
-      const user = await userService.searchByEmail({ username });
-      if (!user || !user.verified) {
-          setErrorResponse('403', 'User account not verified', response);
-          return;
+    try {
+      const user = await userService.searchById(userId)
+      console.log(user)
+      const currentTime = new Date()
+      if(currentTime > user.expirationTime ){
+        res.status(400).json({message: "Link expired"});
       }
-      next();
-  } catch (error) {
-      console.error(error);
-      setErrorResponse('500', 'Internal server error', response);
-  }
-};
+      else{
 
+
+        user.set({...user.dataValues, isVerified: true})
+
+        await user.save();
+        logger.info({
+          message: "User verified successfully", 
+          httpRequest: {
+            requestMethod: req.method,
+            requestUrl: req.originalUrl,
+            status: 204, 
+          }
+        })
+        return res.status(200).json({isVerified: true});
+      }
+      
+      
+    } catch (error) {
+      console.error('Error updating user:', error);
+      logger.error({
+        message: "Error while updating user data", 
+        httpRequest: {
+          requestMethod: req.method,
+          requestUrl: req.originalUrl,
+          status: 500, 
+        }
+    })
+      return res.status(500).json();
+    }
+  
+}
